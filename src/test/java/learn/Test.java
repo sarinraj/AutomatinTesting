6a8109a00e340aa31f27b54d482faa9b087ad530
pc <pc@DESKTOP-1ME8V7C.domain.name>
2021-08-26 11:59:47 +0530

    Wednesday dated on 25/08/2021

--- src/test/java/learn/Test.java
+++ src/test/java/learn/Test.java
@@ -12,33 +12,67 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class Test {
 
 	public static void main(String[] args) {
-		//System.setProperty("webdriver.chrome.driver", "C:\\selenium\\chrome driver\\chromedriver.exe");
-				WebDriverManager.chromiumdriver().setup();
-				WebDriver driver = new ChromeDriver();
-				driver.manage().window().maximize();
-				driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		// System.setProperty("webdriver.chrome.driver", "C:\\selenium\\chrome
+		// driver\\chromedriver.exe");
+		/*
+		 * WebDriverManager.chromiumdriver().setup(); WebDriver driver = new
+		 * ChromeDriver(); driver.manage().window().maximize();
+		 * driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		 * 
+		 * driver.get("https://rahulshettyacademy.com/angularpractice/");
+		 * 
+		 * driver.findElement(By.name("name")).sendKeys("Sarin Raj");
+		 * driver.findElement(By.xpath("//input[@name='email']")).sendKeys(
+		 * "sarinraj50@gmail.com");
+		 * driver.findElement(By.xpath("//input[@type='password']")).sendKeys(
+		 * "sarinraj@123");
+		 * 
+		 * Select Gender = new
+		 * Select(driver.findElement(By.id("exampleFormControlSelect1")));
+		 * Gender.selectByVisibleText("Male");
+		 * //driver.findElement(By.className("form-check-input")).click();
+		 * driver.findElement(By.xpath("//input[@value='option2']")).click();
+		 * driver.findElement(By.name("bday")).sendKeys("05/30/1998");
+		 * driver.findElement(By.xpath("//input[@class='btn btn-success']")).click();
+		 */
 
-				driver.get("https://rahulshettyacademy.com/angularpractice/");
-				
-				driver.findElement(By.name("name")).sendKeys("Sarin Raj");
-				driver.findElement(By.xpath("//input[@name='email']")).sendKeys("sarinraj50@gmail.com");
-				driver.findElement(By.xpath("//input[@type='password']")).sendKeys("sarinraj@123");
-				
-				Select Gender = new Select(driver.findElement(By.id("exampleFormControlSelect1")));
-				Gender.selectByVisibleText("Male");
-				//driver.findElement(By.className("form-check-input")).click();
-				driver.findElement(By.xpath("//input[@value='option2']")).click();
-				driver.findElement(By.name("bday")).sendKeys("05/30/1998");
-				driver.findElement(By.xpath("//input[@class='btn btn-success']")).click();
-				
-				
-				if (driver.findElement(By.className("alert alert-success alert-dismissible")) != ) {
-					System.out.println("Test case pass");
+		/*
+		 * if (driver.findElement(By.className("alert alert-success alert-dismissible"))
+		 * != ) { System.out.println("Test case pass");
+		 * 
+		 * } else { System.out.println("Test case failed"); }
+		 */
+		if (testing() == true) {
+			System.out.println("Test case pass");
+		} else {
+			System.out.println("Test case failed");
+		}
+	}
 
-				} else {
-					System.out.println("Test case failed");
-				}
+	public static boolean testing() {
+		{
+			WebDriverManager.chromiumdriver().setup();
+			WebDriver driver = new ChromeDriver();
+			driver.manage().window().maximize();
+			driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 
+			driver.get("https://rahulshettyacademy.com/angularpractice/");
+
+			driver.findElement(By.name("name")).sendKeys("Sarin Raj");
+			driver.findElement(By.xpath("//input[@name='email']")).sendKeys("sarinraj50@gmail.com");
+			driver.findElement(By.xpath("//input[@type='password']")).sendKeys("sarinraj@123");
+
+			Select Gender = new Select(driver.findElement(By.id("exampleFormControlSelect1")));
+			Gender.selectByVisibleText("Male");
+			// driver.findElement(By.className("form-check-input")).click();
+			driver.findElement(By.xpath("//input[@value='option2']")).click();
+			driver.findElement(By.name("bday")).sendKeys("05/30/1998");
+			driver.findElement(By.xpath("//input[@class='btn btn-success']")).click();
+
+			return true;
+
+		}
+	
 	}
 
 }
